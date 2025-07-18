63483e1298d6fa6b2a901ef2ce3acf1e3755107a
Sabir <magsabir95@gmail.com>
2025-07-18 15:34:30 +0300

    add blogs module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BloggersPlatformModule } from './modules/bloggers-platform/bloggers-platform.module';
+import { BloggersPlatformModule } from './modules/bloggers-platform/bloggers-platform.module';
 
 @Module({
-  imports: [],
+  imports: [BloggersPlatformModule],
   controllers: [AppController],
   providers: [AppService],
 })
